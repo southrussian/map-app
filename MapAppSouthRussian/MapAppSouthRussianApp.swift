5b7ad6644195eab381532abecdbd92c711d6e79f
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-07-08 23:39:40 +0300

    Initial Commit

---
+++ MapAppSouthRussian/MapAppSouthRussianApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSouthRussianApp.swift
+//  MapAppSouthRussian
+//
+//  Created by Danil Peregorodiev on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSouthRussianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
